5f2f35f4dcdb5b9340da12675a8b48e4262647d6
Tuomas Järvensivu <tuomas.jarvensivu@reaktor.fi>
2012-09-19 16:52:57 +0300

    Install dependencies on every build

--- build.sh
+++ build.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
+set -e
+
 which cabal > /dev/null
 if [ "$?" -ne "0" ]; then
 echo "cabal missing. Please install Haskell environment."
   exit 1
 fi
 
+cabal update && cabal install --only-dependencies
 cabal configure && cabal build
